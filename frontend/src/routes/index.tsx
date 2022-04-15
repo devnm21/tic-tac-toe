eb0a5e820a07b3474072f5a25a326f4b1a7c1dd5
Devakumar NM <pydev21@gmail.com>
2022-04-15 19:50:12 +0530

    Add global user context which sets sessionId from server

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,21 +1,32 @@
 // libraries
-import React, { ReactElement } from 'react';
+import React, {ReactElement, useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 // Pages
 import Home from 'views/home';
 import Game from 'views/game';
+import UserContext from '../context/user';
+import { mySession } from '../services/api';
 
 const App: React.FC = (): ReactElement => {
+	const [userState, setUserState] = useState({ sessionId: null });
+
+	useEffect(() => {
+		mySession()
+			.then(({ data }) => setUserState(data));
+	}, []);
+
 	return (
 		<ChakraProvider>
-			<Router>
-				<Routes>[
-					<Route path='/' element={<Home />} />
-					<Route path='/game/:id' element={<Game />} />
-				</Routes>
-			</Router>
+			<UserContext.Provider value={userState}>
+				<Router>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/game/:id' element={<Game />} />
+					</Routes>
+				</Router>
+			</UserContext.Provider>
 		</ChakraProvider>
 	); 
 };
