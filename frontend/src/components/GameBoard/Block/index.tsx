8826ddcc128cb30e0632fe4270b4d7a9174c422d
Devakumar NM <pydev21@gmail.com>
2022-04-16 16:58:46 +0530

    - Add naught and cross assets - Block to show player's turn on mouse hover and proper images for naught and cross

--- frontend/src/components/GameBoard/Block/index.tsx
+++ frontend/src/components/GameBoard/Block/index.tsx
@@ -1,19 +1,70 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement, useEffect, useRef, useState} from 'react';
+import {GridItem} from '@chakra-ui/react';
+import cross from '../../../assets/images/cross.png';
+import naught from '../../../assets/images/naught.png';
 
 
 interface Props {
-	markStyle: object;
+	id: string;
+	onClick: (id: string) => void;
+	markType: string;
+	turnType: string;
 }
 
-const Block: React.FC<Props> = ({ markStyle }: Props): ReactElement => {
+const Block: React.FC<Props> = ({ id, onClick, markType, turnType }: Props): ReactElement => {
+	const [isMouseOver, setIsMouseOver] = useState(false);
+	const [style, setStyle] = useState({});
+
+	const getBackgroundImageStyle = () => {
+		if (markType) {
+			return {
+				cursor: 'not-allowed',
+				backgroundImage: `url(${markType === 'naught' ? naught: cross})`,
+			};
+		}
+		else if (isMouseOver)
+			return {
+				cursor: 'pointer',
+				backgroundImage: `url(${turnType === 'naught' ? naught: cross})`,
+				opacity: 0.6
+			};
+		 return {
+			cursor: 'pointer',
+		 };
+	};
+
+	useEffect(() => {
+		setStyle(getBackgroundImageStyle);
+	}, [isMouseOver, markType	]);
+
 	return  <>
-		<div style={{
-			width: '150px',
-			height: '150px',
-			...markStyle,
-			border: '1px solid red'
-		}} >
-		</div>
+		<GridItem
+			id={id}
+			colSpan={1}
+			bg='white'
+			as={'div'}
+			onClick={() => onClick(id)}
+		>
+			{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+			{/*  @ts-ignore */}
+			<div
+				style={{
+					width: '150px',
+					height: '150px',
+					border: '1px solid red',
+					...style,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: '100%',
+				}}
+				onMouseOver={() => {
+					setIsMouseOver(true);
+				}}
+				onMouseOut={() => {
+					setIsMouseOver(false);
+				}}
+			>
+			</div>
+		</GridItem>
 	</>;
 };
 
