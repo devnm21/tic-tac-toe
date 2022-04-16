2fbb7b52e70d1f580a5bbc081d322a3d259c2bf2
Devakumar NM <pydev21@gmail.com>
2022-04-16 16:56:31 +0530

    Add socket event handlers
    `get-game-moves`, `join`, `player-move`, `game-join`, `player-joined-room`

--- backend/src/socket/game.ts
+++ backend/src/socket/game.ts
@@ -1,26 +1,68 @@
 import { Server, Socket } from 'socket.io';
 import logger from "jet-logger";
-import { parse } from "cookie";
 
+interface Move {
+    markType: string;
+}
+
+interface Moves {
+    [key: string]: Move;
+}
+
+interface State {
+    moves: Moves
+    currentPlayerSessionId: string;
+    joinedPlayerSessionId: string;
+}
+
+interface Dic {
+    [key: string]: State
+}
+
+const roomToGameState : Dic = {};
 class Connection {
      io: Server;
      socket: Socket;
     constructor(io: Server, socket: Socket) {
         this.socket = socket;
         this.io = io;
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        logger.info('session ' + socket.handshake?.session?.id);
-        socket.on('message', (args) => this.sendMessage(args));
 
+        // Socket event that listens to new player requests to get the game moves
+        socket.on('get-game-moves', (room: string) => this.emitGameMovesToSocketsInRoom(room))
+        // Listen to join room (a room represents a game-link/session)
+        socket.on('join', (room: string) => this.joinRoom(room));
+        // Handling every move
+        socket.on('player-move', (data) => this.handlePlayerMove(data))
 
     }
 
-    sendMessage(args: any) {
-        // eslint-disable-next-line no-console
-        console.log('args', args)
-        logger.info('args ' + args)
-        this.io.sockets.emit('message', 'hi');
+    emitGameMovesToSocketsInRoom = (room: string) => {
+        this.io.sockets.in(room)
+            .emit('game-join', roomToGameState[room] || {})
+    }
+
+    joinRoom (room: string) {
+        this.socket.join(room)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.io.to(room).emit('player-joined-room', this.socket.handshake?.session?.id)
+    }
+
+    handlePlayerMove(data: any) {
+        const { move, playerSessionId, roomId } = data;
+            roomToGameState[roomId] = {
+                moves: {
+                    ...roomToGameState[roomId].moves,
+                    [move.blockIndex]: {
+                        markType: move.turnType,
+                    },
+                },
+                currentPlayerSessionId: playerSessionId,
+                joinedPlayerSessionId: roomToGameState[roomId].joinedPlayerSessionId || '',
+            };
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        this.io.to(roomId).emit('player-move',
+            { blockIndex: move.blockIndex, turnType: move.turnType, playerSessionId });
     }
 
 }
@@ -29,6 +71,18 @@ function chat(io: Server) {
     io.on('connection', (socket) => {
         new Connection(io, socket);
     });
+}
+
+export const initGameState = (roomId: string, currentPlayerSessionId: string) => {
+    roomToGameState[roomId] = {
+        moves: {},
+        currentPlayerSessionId,
+        joinedPlayerSessionId: '',
+    };
+};
+
+export const updateGamePlayerJoin = (roomId: string, joinedSessionId: string) => {
+    roomToGameState[roomId].joinedPlayerSessionId = joinedSessionId
 }
 
 export default chat;
